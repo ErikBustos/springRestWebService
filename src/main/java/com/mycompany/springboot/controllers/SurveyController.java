545a21ac873e2de8e941f245326f3550a8d84a57
erikbustosmo@gmail.com <[erikbustosmo@gmail.com]>
2022-04-16 13:42:47 -0500

    added support to add a question to a survey

--- src/main/java/com/mycompany/springboot/controllers/SurveyController.java
+++ src/main/java/com/mycompany/springboot/controllers/SurveyController.java
@@ -1,11 +1,16 @@
 package com.mycompany.springboot.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mycompany.springboot.models.Question;
 import com.mycompany.springboot.service.SurveyService;
@@ -19,7 +24,23 @@ public class SurveyController {
 	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
 		return surveyService.retrieveQuestions(surveyId);
 	}
-	
-	
+
+	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
+	public Question retrieveQuestionsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
+		return surveyService.retrieveQuestion(surveyId, questionId);
+	}
+
+	@PostMapping("/surveys/{surveyId}/questions")
+	public ResponseEntity<?> addQuestionsToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+		Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+		if(question == null)
+			return ResponseEntity.noContent().build();
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(question.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
 	
 }

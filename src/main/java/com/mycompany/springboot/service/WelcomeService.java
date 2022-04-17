410ec4ab90a947b69438ec377ad063e241e94c78
erikbustosmo@gmail.com <[erikbustosmo@gmail.com]>
2022-04-17 13:33:04 -0500

    added properties files support

--- src/main/java/com/mycompany/springboot/service/WelcomeService.java
+++ src/main/java/com/mycompany/springboot/service/WelcomeService.java
@@ -1,10 +1,15 @@
 package com.mycompany.springboot.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WelcomeService {
+
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+
 	public String retrieveWelcomeMessage( ) {
-		return "Good Morning updated22";
+		return welcomeMessage;
 	}
 }

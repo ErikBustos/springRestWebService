a65897e56685fa4e370e952b7a0a54ef6fb53197
Erik Bustos <@BustEr01>
2022-04-20 17:26:48 -0500

    added addQuestion Testcase

--- src/test/java/com/mycompany/springboot/controllers/SurveyControllerIT.java
+++ src/test/java/com/mycompany/springboot/controllers/SurveyControllerIT.java
@@ -3,8 +3,10 @@ package com.mycompany.springboot.controllers;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.List;
 
 import com.mycompany.springboot.Application;
+import com.mycompany.springboot.models.Question;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +14,7 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -19,6 +22,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 //Initialize and launch Spring Boot Application
 @RunWith(SpringRunner.class)
@@ -51,6 +55,54 @@ public class SurveyControllerIT {
 		String expected = "{id:Question1,description:Largest Country in the World,correctAnswer:Russia}";
 
 		//JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+	@Test
+	public void retrieveAllSurveyQuestions() throws Exception {
+
+		String url = "http://localhost:" + port + "/surveys/Survey1/questions";
+
+		TestRestTemplate restTemplate = new TestRestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+		ResponseEntity<List<Question>> response = restTemplate.exchange(url,
+				HttpMethod.GET, new HttpEntity<String>("DUMMY_DOESNT_MATTER",
+						headers),
+				new ParameterizedTypeReference<List<Question>>() {
+				});
+
+		Question sampleQuestion = new Question("Question1",
+				"Largest Country in the World", "Russia", Arrays.asList(
+						"India", "Russia", "United States", "China"));
+
+		assertTrue(response.getBody().contains(sampleQuestion));
+    }
+
+    @Test
+    public void addQuestion() {
+        String url = "http://localhost:" + port + "/surveys/Survey1/questions";
+
+        Question question = new Question("DOESNTMATTER",
+        "Largest Country in the World", "Russia", Arrays.asList(
+                "India", "Russia", "United States", "China"));;
+
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<Question> entity = new HttpEntity<Question>(question, headers);
+        
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+        String actualLocation = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+        System.out.println(actualLocation);
+        assertTrue(actualLocation.contains("/surveys/Survey1/questions"));
+        assertTrue(response.getStatusCode().equals(HttpStatus.CREATED));
 
     }
 }
